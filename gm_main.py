0e402d9fe2376a95c922ed90d6c9f03be210f3d2
Aroha Kiri <arohakiri@gmail.com>
2026-01-13 16:11:02 +1300

    feat: gm_main skeleton

--- gm_main.py
+++ gm_main.py
@@ -1,1 +1,27 @@
 # gm_main.py
+# This script is initialised by the Godot game whenever a match begins. it handles 1 match between 2 python scripts.
+# It also provides methods for retrieving match stats, initializing to default state, updating leaderboard,
+# Reading & retrieving leaderboard, changing match settings/vars.
+
+# Author: AROHA KIRI
+# 2026
+# Tuhura Tech
+
+import importlib.util
+import sys
+from pathlib import Path
+
+# Constants
+STEAL = 0
+SUPPORT = 1
+
+# Variables
+p1_main
+p2_main
+
+# function: init()
+# Returns this file to it's default state. Must be provided 2 python files or will not work.
+def init():
+    #init somewhat
+    pass
+
