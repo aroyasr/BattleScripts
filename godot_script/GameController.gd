fbd854aceb1481bc2425031aac3dbbda568052fb
aroyasr <kiriaroh@myvuw.ac.nz>
2026-01-20 16:02:46 +1300

    feat: Match setup screen, button functionality

--- godot_script/GameController.gd
+++ godot_script/GameController.gd
@@ -1,21 +1,20 @@
-# root.gd
-
+# GameController.gd
 # This script manages the currently active scene. It also holds global variables.
 class_name GameController extends Node
 
 # Variables
-var game_controller : GameController
 @onready var ui = get_node("current_ui_scene")
 var current_ui_scene 
+var current_state
 
 # Functions
 func _ready() -> void:
-	Root.game_controller = self
 	change_ui_scene("res://scenes/main_menu.tscn")
 
-func change_ui_scene(new_scene: String) -> void:
-	if current_ui_scene != null:
-		current_ui_scene.queue_free() # Remove node entirely
-	var new = load(new_scene).instantiate()
-	ui.add_child(new)
-	current_ui_scene = new
+func change_ui_scene(new_scene_path: String):
+	var old_scene = ui.get_child(0)
+	var new_scene = load(new_scene_path).instantiate()
+	current_ui_scene = new_scene
+	ui.add_child(new_scene)
+	if old_scene != null:
+		old_scene.queue_free()
---
+++ godot_script/match_setup.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_button_back_pressed() -> void:
+	Root.main_menu()
